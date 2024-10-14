3dbe996e64ce9746edbfbff8774f5ccb85559893
Erie <zcapyfa@ucl.ac.uk>
2024-10-14 11:22:15 +0100

    Format the file

--- group.py
+++ group.py
@@ -2,37 +2,35 @@
 
 # Your code to go here...
 
-my_group = {'Jill': 
-            {'age': 26, 
-            'job': 'biologist', 
-            'connection':{
-                'friend': 'Zalika',
-                'partner': 'John'
-            }
+my_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "connection": {
+            "friend": "Zalika", 
+            "partner": "John"
             },
-            'Zalika':
-            {'age': 28,
-             'job': 'artist',
-             'connection':{
-                 'friend': 'Jill'
-             }
-             },
-             'John':
-             {'age': 27,
-              'job': 'writer',
-              'connection': {
-                  'partner': 'Jill'
-              }
-              },
-            'Nash':
-            {'age': 34,
-             'job': 'chef',
-             'connection': {
-                 'cousin': 'John',
-                 'landlord': 'Zalika'
-             }
-
-            }  
+    },
+    "Zalika": {
+        "age": 28, 
+        "job": "artist", 
+        "connection": {
+            "friend": "Jill"
+            }
+    },
+    "John": {
+        "age": 27, 
+        "job": "writer", 
+        "connection": {
+            "partner": "Jill"
+            }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "connection": {
+            "cousin": "John", 
+            "landlord": "Zalika"
+            },
+    },
 }
-
-
